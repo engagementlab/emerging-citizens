171ceb06b2ee43be84b27847d6a5153618e350f4
Johnny Richardson <johnny.c.rich@gmail.com>
2016-06-21 11:14:39 -0400

    Production compilation and CDN path fixes.

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -11,7 +11,7 @@ module.exports = function() {
   // Files to minify
   config.target.files[fileOut] = [
 
-    __dirname + '/../public/styles/cmp.css', // site SCSS
+    __dirname + '/../public/styles/core.css', // site SCSS
   
   ];
 
