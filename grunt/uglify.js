f3d63499a22ac61ca22b4504e02401cca6368944
Johnny Richardson <johnny.c.rich@gmail.com>
2016-06-23 14:08:15 -0400

    Include minified nosleep and decrease its refresh duration.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -12,7 +12,7 @@ module.exports = function() {
     __dirname + '/../public/js/jquery/*.js', // jquery
     __dirname + '/../public/plugins/*.js',  // Plugins
     __dirname + '/../public/plugins/**/*.js',
-    '!'+__dirname + '/../public/bower_components/nosleep/NoSleep.js',
+    '!' + __dirname + '/../public/bower_components/nosleep/*.js', // Ignore nosleep
     __dirname + '/../public/bower_components/progressbar.js/dist/progressbar.min.js',
     __dirname + '/../public/bower_components/ion-sound/js/ion.sound.min.js',
     __dirname + '/../public/gsap-timeline-slider.js',
