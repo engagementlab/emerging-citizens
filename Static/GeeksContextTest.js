beffc39a1b7f5b6c42cf8a6f29d62264ea45d503
matt benson <mattwbens@gmail.com>
2016-06-23 12:42:07 -0400

    AnimContextLayout

--- Static/GeeksContextTest.js
+++ Static/GeeksContextTest.js
@@ -1,4 +1,9 @@
 //Define vars/functions/objects
 
+var $topMenu = $("#menuImage");
+var $contextPane = $("#contextPane");
 
 
+//code body
+TweenMax.from($topMenu, .5, {y:-300, ease:Circ.easeOut});
+TweenMax.from($contextPane, .5, {rotation: 180, y:500});
