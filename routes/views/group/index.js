839b7efaf72dc52b14b623162ebb0e05074299b9
Johnny Richardson <johnny.c.rich@gmail.com>
2016-09-19 18:24:35 -0400

    Finished filtering vulgar words out of potential room code.

--- routes/views/group/index.js
+++ routes/views/group/index.js
@@ -1,17 +1,18 @@
 /**
- * Emerging Citizens
- * Developed by Engagement Lab, 2016
- * ==============
- * Moderator view controller.
- *
- * Help: http://keystonejs.com/docs/getting-started/#routesviews-firstview
- *
- * @class group
- * @static
- * @author Johnny Richardson
- *
- * ==========
- */
+* Emerging Citizens
+* Developed by Engagement Lab, 2016
+* ==============
+* Moderator view controller.
+*
+* Help: http://keystonejs.com/docs/getting-started/#routesviews-firstview
+*
+* @class group
+* @static
+* @author Johnny Richardson
+*
+* ==========
+*/
+
 var keystone = require('keystone'),
     randomstring = require('randomstring'),
     GameSession = keystone.list('GameSession'),
@@ -39,43 +40,49 @@ exports = module.exports = function(req, res) {
     // item in the header navigation.
     locals.section = 'group';
 
-    function checkCode() {
-        var code;
+    // Prevents generated code from being vulgar
+    function checkCode(code, callback) {
 
-        var badCode = {'ANUS', 'ARSE', 'CLIT', 'COCK', 'COON', 'CUNT', 'DAGO', 'DAMN', 'DICK', 'DIKE', 'DYKE', 'FUCK', 
-        'GOOK', 'HEEB', 'HECK', 'HELL', 'HOMO', 'JIZZ', 'KIKE', 'KUNT', 'KYKE', 'LICK', 'MICK', 'MUFF', 'PAKI', 'PISS', 'POON', 'PUTO', 'SHIT', 'SHIZ', 'SLUT', 'SMEG', 'SPIC', 'TARD', 'TITS', 'TWAT', 'WANK'};
+        var badWords = ['ANUS', 'ARSE', 'CLIT', 'COCK', 'COON', 'CUNT', 'DAGO', 'DAMN', 'DICK', 'DIKE', 'DYKE', 'FUCK', 
+                        'GOOK', 'HEEB', 'HECK', 'HELL', 'HOMO', 'JIZZ', 'KIKE', 'KUNT', 'KYKE', 'LICK', 'MICK', 'MUFF',
+                        'PAKI', 'PISS', 'POON', 'PUTO', 'SHIT', 'SHIZ', 'SLUT', 'SMEG', 'SPIC', 'TARD', 'TITS', 'TWAT', 
+                        'WANK', 'POOP', 'FUCK', 'FCUK', 'FUKK', 'KILL', 'JERK', 'CRAP', 'FOOK', 'DORK', 'DOPE', 'DUNG',
+                        'FAGS', 'FART', 'GAYS', 'HELL', 'HOAR', 'JAPS', 'NAZI', 'ORGY', 'PORN', 'PUKE', 'RAPE', 'SEXY',
+                        'SPIG', 'SUCK', 'STAB', 'SMUT', 'SPAZ', 'TWIT'];
+
+        if (_.contains (badWords, code))
+            checkCode(generateCode(), callback);
+        else
+            callback(code);
 
     }
 
     function generateCode() {
-
         
         var code = randomstring.generate({ length: 4, charset: 'alphabetic' }).toUpperCase();
+        return code;
 
-        if (_.contains (badCode, code)) {
-            console.log ("finding new, clean code");
-            code = randomstring.generate({ length: 4, charset: 'alphabetic' }).toUpperCase();
-        } else {
-            return code;
-        }
-
-        
-    
     }
 
     view.on('init', function(next) {
 
-        
-        gameCode = generateCode();
+        var generatedCode = generateCode();
 
-        // Check if there's already a game with the generated access code
-        GameSession.model.findOne({accessCode: gameCode}, function (err, session) {
+        // Make sure code is not vulgar and then check if not used already
+        checkCode(generatedCode, function(code) {
 
-            // There is! A one in 15,000 probability! Make a new one
-            if(session !== null)
-                gameCode = generateCode();
+            gameCode = code;
 
-            next();
+            // Check if there's already a game with the generated access code
+            GameSession.model.findOne({accessCode: gameCode}, function (err, session) {
+
+                // There is! A one in 15,000 probability! Make a new one
+                if(session !== null)
+                    gameCode = generateCode();
+
+                next();
+
+            });
 
         });
 
