a2eb59481de8ab4e8a32d2919b2493fbc026ae06
Johnny Richardson <johnny.c.rich@gmail.com>
2016-09-19 17:54:03 -0400

    Grunt bump task added.

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,15 +1,34 @@
-module.exports = {
+module.exports = function(grunt, options) {
 
-  options: {
-    files: ['package.json'],
-    commit: true,
-    commitMessage: 'Production Release v%VERSION%',
-    commitFiles: ['package.json'],
-    createTag: true,
-    tagName: 'v%VERSION%',
-    tagMessage: 'Version %VERSION%',
-    push: true,
-    pushTo: 'origin'
-  }
+	var 
+	production = {
+	  options: {
+	    commit: false,
+	    createTag: true,
+	    tagName: 'v%VERSION%',
+	    tagMessage: 'Production Version %VERSION%',
+	    push: true,
+	    pushTo: 'origin'
+	  }
+	},
+
+	staging = {
+	  options: {
+	    files: [__dirname + '/../package.json'],
+	    commit: true,
+	    commitMessage: 'Staging Release v%VERSION%',
+	    commitFiles: [__dirname + '/../package.json'],
+	    createTag: true,
+	    tagName: 'v%VERSION%',
+	    tagMessage: 'Version %VERSION%',
+	    push: true,
+	    pushTo: 'origin'
+	  }
+	}
+
+	return {
+		'production': production,
+		'staging': staging
+	};
       
 };
