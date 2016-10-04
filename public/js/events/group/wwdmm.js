66dcb17f9481fe1ac145d87d7353ddd25728e560
Johnny Richardson <johnny.c.rich@gmail.com>
2016-10-04 13:11:56 -0400

    Resolved scoring and animation issues for WWDMM.

--- public/js/events/group/wwdmm.js
+++ public/js/events/group/wwdmm.js
@@ -172,104 +172,108 @@ var gameEvents = function(eventId, eventData) {
 	  case 'meme:results':
 
       updateGameContent(eventData, function() {
+        
         loadMemes();
-      });
 
-      function showScores() {
+        function showScores() {
 
-        $('#submissions').fadeOut(500).remove();
-        
-        var scoresAnim = new TimelineLite({onComplete: function() { showLeaderboard(); } });
-        
-        scoresAnim.from($('#scores'), 1, {autoAlpha:0}).add('scores')
-        .to($('#scores'), 1, {autoAlpha:0, scale: 0, display: 'none', ease:Bounce.easeOut}, 'scores+=4');
-
-      }
-
-      function showLeaderboard() {
-        
-        var leaderboardAnim = new TimelineLite({onComplete: function() { nextRound(); } });
-        
-        leaderboardAnim
-        .to($('#round-header'), .5, {autoAlpha:0, y:'0%', display:'none'}).add('header')
-        .fromTo($('#leaderboard-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
-
-        .from($('#leaderboard'), 1, {autoAlpha:0}).add('leaderboard')
-        
-        .to($('#leaderboard'), 1, {autoAlpha:0, scale:0, display:'none', ease:Bounce.easeOut}, 'leaderboard+=4');
-
-      }
-
-      function nextRound() {
-
-        var nextRoundAnim = new TimelineLite();
-
-        // Show countdown
-        if($('#next-round')[0]) {
+          $('#submissions').fadeOut(500).remove();
           
-          nextRoundAnim
-          .to($('#leaderboard-header'), .5, {autoAlpha:0, y:'50%', display:'none'}).add('header')
-          .fromTo($('#next-round-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
+          var scoresAnim = new TimelineLite({onComplete: function() { showLeaderboard(); } });
+          
+          scoresAnim.from($('#scores'), 1, {autoAlpha:0}).add('scores')
+          .to($('#scores'), 1, {autoAlpha:0, scale: 0, display: 'none', ease:Bounce.easeOut}, 'scores+=4');
+
+        }
+
+        function showLeaderboard() {
+          
+          var leaderboardAnim = new TimelineLite({onComplete: function() { nextRound(); } });
+          
+          leaderboardAnim
+          .to($('#round-header'), .5, {autoAlpha:0, y:'0%', display:'none'}).add('header')
+          .fromTo($('#leaderboard-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
+
+          .from($('#leaderboard'), 1, {autoAlpha:0}).add('leaderboard')
+          
+          .to($('#leaderboard'), 1, {autoAlpha:0, scale:0, display:'none', ease:Bounce.easeOut}, 'leaderboard+=4');
+
+        }
+
+        function nextRound() {
+
+          var nextRoundAnim = new TimelineLite();
+
+          // Show countdown
+          if($('#next-round')[0]) {
             
-          TweenLite.from($('#next-round, #game-ended'), 1, { autoAlpha: 0, scale: 0 });
+            nextRoundAnim
+            .to($('#leaderboard-header'), .5, {autoAlpha:0, y:'50%', display:'none'}).add('header')
+            .fromTo($('#next-round-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
+              
+            TweenLite.from($('#next-round, #game-ended'), 1, { autoAlpha: 0, scale: 0 });
+
+          }
+          else {
+
+            var secondsLeft = 10;
+
+            nextRoundAnim
+            .to($('#leaderboard-header'), .5, {autoAlpha:0, y:'0%', display:'none'}).add('header')
+            .fromTo($('#winners-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
+            .from($('#winners-circle'), 1, {autoAlpha:0});
+
+            setInterval(function() {
+                secondsLeft--;
+
+                // End countdown
+                if(secondsLeft == 0)
+                  location.href = '\\';
+            }, 1000);
+            
+          }
 
         }
-        else {
 
-          var secondsLeft = 10;
+        var submissionsAnim = new TimelineLite({onComplete: function() { showScores(); }});
 
-          nextRoundAnim
-          .to($('#leaderboard-header'), .5, {autoAlpha:0, y:'0%', display:'none'}).add('header')
-          .fromTo($('#winners-header'), .5, {autoAlpha:0, y:'50%'}, {autoAlpha:1, y:'0%', display:'block'}, 'header+=.6')
-          .from($('#winners-circle'), 1, {autoAlpha:0});
+        submissionsAnim
+        .from($('#submissions'), 1, {autoAlpha:0}).add('submissions');
 
-          setInterval(function() {
-              secondsLeft--;
+        debugger;
+        
+        var elements = $('#submissions .submission');
 
-              // End countdown
-              if(secondsLeft == 0)
-                location.href = '\\';
-          }, 1000);
-          
-        }
+        // 1) Show submissions
+        _.each(elements, function(el, index) {
 
-      }
+            submissionsAnim
+            .from(el, 1, {y:0, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
+            
+            .from($(el).find('.meme'), 1, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
+            
+            .from($(el).find('.votes-box'), .5, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
+            .staggerFrom($(el).find('.votes-box .vote'), .3, {scale: 0, autoAlpha:0, ease:Bounce.easeOut}, .3, '+=.4')
+            .staggerFrom($(el).find('.votes-box .vote .score'), 1, {scale: 0, autoAlpha:0, rotation:360, ease:Elastic.easeOut}, 1, '+=.4')
 
-      var submissionsAnim = new TimelineLite({onComplete: function() { showScores(); }});
+            .from($(el).find('.name.submitter'), 1, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=2')
 
-      submissionsAnim
-      .from($('#submissions'), 1, {autoAlpha:0}).add('submissions');
-      
-      var elements = $('#submissions .submission');
+            .from($(el).find('.likes-box'), .5, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
+            .staggerFrom($(el).find('.likes-box .vote'), .3, {scale: 0, autoAlpha:0, ease:Bounce.easeOut}, .3, '+=.4')
+            .staggerFrom($(el).find('.likes-box .vote .score'), 1, {scale: 0, autoAlpha:0, rotation:-180, ease:Elastic.easeOut}, 1, '+=.4')
 
-      // 1) Show submissions
-      _.each(elements, function(el, index) {
+            .to(el, 1, {y:0, scale: 0, autoAlpha:0, display:'none', ease:Ease.easeIn}, '+=4');
 
-          submissionsAnim
-          .from(el, 1, {y:0, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
-          
-          .from($(el).find('.meme'), 1, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
-          
-          .from($(el).find('.votes-box'), .5, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
-          .staggerFrom($(el).find('.votes-box .vote'), .3, {scale: 0, autoAlpha:0, ease:Bounce.easeOut}, .3, '+=.4')
-          .staggerFrom($(el).find('.votes-box .vote .score'), 1, {scale: 0, autoAlpha:0, rotation:360, ease:Elastic.easeOut}, 1, '+=.4')
-
-          .from($(el).find('.name.submitter'), 1, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=2')
-
-          .from($(el).find('.likes-box'), .5, {y:100, scale: 0, autoAlpha:0, ease:Ease.easeOut}, '+=.5')
-          .staggerFrom($(el).find('.likes-box .vote'), .3, {scale: 0, autoAlpha:0, ease:Bounce.easeOut}, .3, '+=.4')
-          .staggerFrom($(el).find('.likes-box .vote .score'), 1, {scale: 0, autoAlpha:0, rotation:-180, ease:Elastic.easeOut}, 1, '+=.4')
-
-          .to(el, 1, {y:0, scale: 0, autoAlpha:0, display:'none', ease:Ease.easeIn}, '+=4');
-
-      });
-
-      // Debugging
-      if($('#slider-gsap').length) {
-        resultsAnimSlider = new GSAPTLSlider(submissionsAnim, "slider-gsap", {
-            width: 600
         });
-      }
+
+        // Debugging
+        if($('#slider-gsap').length) {
+          resultsAnimSlider = new GSAPTLSlider(submissionsAnim, "slider-gsap", {
+              width: 600
+          });
+        }
+        
+      });
 
 	    break;
 
